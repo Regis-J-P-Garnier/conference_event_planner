b6969217bd30443a04c2826669875ee3346350bc
RJPG <regis.garnier@wanadoo.fr>
2024-10-09 17:01:43 +0200

    corrected code

--- src/avSlice.js
+++ src/avSlice.js
@@ -1,22 +1,57 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-export const avSlice = createSlice({
+const projectorsImgUrl = 'images/projector-6760349_1280.jpg';
+const speakersImgUrl  =  'images/speakers-2265542_1280.jpg';
+const microphonesImgUrl  =  'images/studio-microphone-2233079_1280.jpg';
+const whiteboardsImgUrl  =  'images/canvas-8325881_1280.webp';
+const signagesImgUrl  =  'images/signage-1225355_1280.jpg';
+const avSlice = createSlice({
   name: "av",
   initialState: [
-    
+        {
+        img: projectorsImgUrl,
+        name: "Projectors",
+        cost: 200,
+        quantity: 0,
+    },
+    {
+        img: speakersImgUrl,
+        name: "Speaker",
+        cost: 35,
+        quantity: 0,
+    },
+    {
+        img: microphonesImgUrl,
+        name: "Microphones",
+        cost: 45,
+        quantity: 0,
+    },
+    {
+        img: whiteboardsImgUrl,
+        name: "Whiteboards",
+        cost: 80,
+        quantity: 0,
+    },
+    {
+        img: signagesImgUrl,
+        name: "Signage",
+        cost: 80,
+        quantity: 0,
+    },
   ],
-
-
   reducers: {
     incrementAvQuantity: (state, action) => {
-      
+        const item = state[action.payload];
+        if (item) {
+            item.quantity++;
+        }
     },
     decrementAvQuantity: (state, action) => {
-     
+        const item = state[action.payload];
+        if (item && item.quantity > 0) {
+            item.quantity--;
+        }
     },
   },
 });
-
 export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
-
 export default avSlice.reducer;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
 import venueReducer from './venueSlice';
-
+import avReducer from './avSlice'; // warning JS or JSx ?
+import mealsReducer from './mealsSlice';
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
   },
 });
